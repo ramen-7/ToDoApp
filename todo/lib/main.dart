458a824e40969c6b074636bcf1fb0aeaadf2e65a
Shivam Taneja <76044999+ramen-7@users.noreply.github.com>
2022-05-11 01:05:37 +0530

    final build

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:what_todo/screens/homepage.dart';
 
-void main()
-{
+void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget
-{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          child: Text("Hello World!"),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
         ),
-      )
+      ),
+      home: Homepage(),
     );
   }
 }
